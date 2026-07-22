b2d25a28a2c259b31c6d9ef2960a510477cf4066
nourabosaif <abosaif.nany@gmail.com>
2026-07-22 10:28:35 +0300

    grid skeleton

---
+++ ecommerce_task/src/components/ProductGridSkeleton.tsx
@@ -0,0 +1,11 @@
+import { ProductCardSkeleton } from "./ProductCardSkeleton";
+
+export function ProductGridSkeleton({ count = 8 }: { count?: number }) {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      {Array.from({ length: count }).map((_, i) => (
+        <ProductCardSkeleton key={i} />
+      ))}
+    </div>
+  );
+}
