ae7c905c30043e245c8f323a04c14da4d318d5f2
nourabosaif <abosaif.nany@gmail.com>
2026-07-22 10:15:13 +0300

    api layer

---
+++ ecommerce_task/src/api/products.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import type { Product, ProductsResponse } from "../types/product";
+
+const BASE_URL = "https://dummyjson.com/products";
+
+interface GetProductsParams {
+  limit: number;
+  skip: number;
+  search?: string;
+  category?: string;
+}
+
+export async function getProducts({
+  limit,
+  skip,
+  search,
+  category,
+}: GetProductsParams): Promise<ProductsResponse> {
+  let url = `${BASE_URL}`;
+
+  if (search) {
+    url = `${BASE_URL}/search`;
+  } else if (category) {
+    url = `${BASE_URL}/category/${category}`;
+  }
+
+  const { data } = await axios.get<ProductsResponse>(url, {
+    params: {
+      limit,
+      skip,
+      ...(search ? { q: search } : {}),
+    },
+  });
+
+  return data;
+}
+
+export async function getCategories(): Promise<string[]> {
+  const { data } = await axios.get<string[]>(`${BASE_URL}/categories`);
+  return data;
+}
